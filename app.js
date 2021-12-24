d704ac42e1361888ce1f9be9a4de9f874c942a80
lotus9630 <lotus9630@gmail.com>
2021-12-25 07:20:05 +0900

    change PORT

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.set("port", 3000);
+app.set("port", 8080);
 
 app.get("/", (req, res) => {
   res.send("hello, world");
 });
 
-app.listen(app.get("port"), () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
 });
